588b7d094dff9429c3b37c419c66bab7ad6bb1c3
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-07-06 21:53:12 -0400

    [feat] add clap and rewrite

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,30 +1,40 @@
 use ast_grep_core::AstGrep;
-use std::env::args;
 use std::fs::read_to_string;
 use std::io::Result;
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(value_parser)]
+    path: String,
+    #[clap(short, long, value_parser)]
+    expression: String,
+    #[clap(short, long, value_parser)]
+    rewrite: Option<String>,
+}
 
 fn main() -> Result<()> {
-    let mut input = String::new();
-    let mut pattern = String::new();
-    for arg in args().skip(1) {
-        if arg.starts_with("-e") {
-            pattern = arg
-                .split_once('=')
-                .expect("invalid command line argument")
-                .1
-                .to_string();
-        } else {
-            input = read_to_string(arg)?;
-        }
-    }
+    let args = Args::parse();
+    let pattern = args.expression;
+    let input = read_to_string(&args.path)?;
     let grep = AstGrep::new(input);
     let matches = grep.root().find_all(&*pattern);
     if matches.is_empty() {
         println!("pattern not found!");
         return Ok(());
     }
-    for e in matches {
-        println!("{}", e.text());
+    println!("{}", args.path);
+    if let Some(rewrite) = args.rewrite {
+        for mut e in matches {
+            println!("------------------");
+            println!("{}", e.replace(&pattern, &rewrite).unwrap().inserted_text);
+        }
+    } else {
+        for e in matches {
+            println!("------------------");
+            println!("{}", e.text());
+        }
     }
     Ok(())
 }
