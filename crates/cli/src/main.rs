8723e5843ac9dcfde9ed0dd86178558ace0def94
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-07-08 01:13:39 -0400

    [feat] update args

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -5,18 +5,33 @@ use clap::Parser;
 
 
 #[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+/**
+ * TODO: add some description for ast-grep: sg
+ * Example:
+ * sg -e
+ */
 struct Args {
-    #[clap(value_parser)]
-    path: String,
-    #[clap(short, long, value_parser)]
-    expression: String,
+    /// AST pattern to match
+    #[clap(short,long,value_parser)]
+    pattern: String,
+
+    /// String to replace the matched AST node
     #[clap(short, long, value_parser)]
     rewrite: Option<String>,
+
+    /// A comma-delimited list of file extensions to process.
+    #[clap(short, long)]
+    extensions: Vec<String>,
+
+    /// The path whose descendent files are to be explored.
+    #[clap(value_parser, default_value=".")]
+    path: String,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let pattern = args.expression;
+    let pattern = args.pattern;
     let input = read_to_string(&args.path)?;
     let grep = AstGrep::new(input);
     let matches = grep.root().find_all(&*pattern);
