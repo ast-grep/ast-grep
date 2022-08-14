cc3bc3652b56e09a187c88eb39d04423e3f869ce
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-08-13 23:52:48 -0400

    [feat] add base url

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/ast-grep/',
   server: {
     fs: {
       // Allow serving files from one level up to the project root
