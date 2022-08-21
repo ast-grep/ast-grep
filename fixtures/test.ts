0f89d9122af50a1525732fb5483b5e49249eed71
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-08-20 22:29:05 -0400

    [chore] add some examples

--- fixtures/test.ts
+++ fixtures/test.ts
@@ -1,25 +1,33 @@
-let a = 123
+// let a = 123
 
 
-async function foo(things) {
-  const results = [];
-  for (const thing of things) {
-    // Bad: each loop iteration is delayed until the entire asynchronous operation completes
-    results.push(await bar(thing));
-  }
-  return baz(results);
-}
+// async function foo(things) {
+//   const results = [];
+//   for (const thing of things) {
+//     // Bad: each loop iteration is delayed until the entire asynchronous operation completes
+//     results.push(await bar(thing));
+//   }
+//   return baz(results);
+// }
 
-if (false) {
-  console.log('123')
-}
+// if (false) {
+//   console.log('123')
+// }
 
-class Test {
-  set test(test) {
-    return 'shoud hit'
-  }
+// class Test {
+//   set test(test) {
+//     return 'shoud hit'
+//   }
 
-  test(test) {
-    return 'shoud not hit'
-  }
-}
+//   test(test) {
+//     return 'shoud not hit'
+//   }
+// }
+
+Promise.all([
+  await p1,  // match
+  p2,        // no match
+  another(await p1), // no match
+  await p4,  // match
+  ...[1,2,3].map(async (i) => await Promise.resolve(i)) // no match
+])
