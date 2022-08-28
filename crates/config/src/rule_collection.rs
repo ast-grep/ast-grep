27bb73d0f70d9621a33132eb131f205ec48684c1
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-08-28 01:02:42 -0400

    [feat] add rule colllection

--- crates/config/src/rule_collection.rs
+++ crates/config/src/rule_collection.rs
@@ -1,21 +1,57 @@
 use crate::RuleConfig;
 use ast_grep_core::language::Language;
+
 use std::collections::HashMap;
 
 pub struct RuleBucket<L: Language> {
-    rules: Vec<Vec<RuleConfig<L>>>,
+    rules: Vec<RuleConfig<L>>,
+}
+
+impl<L: Language> RuleBucket<L> {
+    pub fn add(&mut self, rule: RuleConfig<L>) {
+        self.rules.push(rule);
+    }
+}
+
+impl<L: Language> Default for RuleBucket<L> {
+    fn default() -> Self {
+        Self { rules: vec![] }
+    }
 }
 
 /// A collection of rules to run one round of scanning.
 /// Rules will be grouped together based on their language, path globbing and pattern rule.
-pub struct RuleConfigCollection<L: Language> {
+pub struct RuleCollection<L: Language> {
     // TODO: use tinyvec
     pub tenured: HashMap<String, RuleBucket<L>>,
     pub contingent: Vec<RuleConfig<L>>,
 }
 
-impl<L: Language> RuleConfigCollection<L> {
+impl<L: Language + ToString> RuleCollection<L> {
     pub fn new(configs: Vec<RuleConfig<L>>) -> Self {
-        todo!()
+        let mut tenured = HashMap::<String, RuleBucket<L>>::new();
+        let mut contingent = vec![];
+        for config in configs {
+            if config.files.is_none() && config.ignores.is_none() {
+                let lang = config.language.to_string();
+                tenured.entry(lang).or_default().add(config);
+            } else {
+                contingent.push(config);
+            }
+        }
+        Self {
+            tenured,
+            contingent,
+        }
+    }
+
+    // TODO: get rules without allocation
+    pub fn get_rules_for_lang(&self, lang: &L) -> Vec<&RuleConfig<L>> {
+        let lang = lang.to_string();
+        if let Some(bucket) = self.tenured.get(&lang) {
+            bucket.rules.iter().collect()
+        } else {
+            vec![]
+        }
     }
 }
