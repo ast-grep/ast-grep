b978986f17245776cf0670e8a056ceb726c2b7cf
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-09-06 01:02:04 -0400

    [chore] add npm cli package

---
+++ npm/move-artifacts.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+const dir = `${__dirname}/..`;
+
+// Add `libc` fields only to platforms that have libc(Standard C library).
+const triples = [
+  {
+    name: 'x86_64-apple-darwin',
+  },
+  {
+    name: 'x86_64-unknown-linux-gnu',
+  },
+  {
+    name: 'x86_64-pc-windows-msvc',
+  },
+  {
+    name: 'aarch64-apple-darwin',
+  },
+];
+const cpuToNodeArch = {
+  x86_64: 'x64',
+  aarch64: 'arm64',
+};
+const sysToNodePlatform = {
+  linux: 'linux',
+  darwin: 'darwin',
+  windows: 'win32',
+};
+
+for (let triple of triples) {
+  let [cpu, , os, abi] = triple.name.split('-');
+  cpu = cpuToNodeArch[cpu] || cpu;
+  os = sysToNodePlatform[os] || os;
+
+  let t = `${os}-${cpu}`;
+  if (abi) {
+    t += '-' + abi;
+  }
+
+  buildCLI(triple.name, os, t);
+}
+
+
+function buildCLI(triple, os, t) {
+  let binary = os === 'win32' ? 'sg.exe' : 'sg';
+  fs.copyFileSync(`${dir}/artifacts/bindings-${triple}/${binary}`, `${dir}/npm/platforms/${t}/${binary}`);
+  fs.chmodSync(`${dir}/npm/platforms/${t}/${binary}`, 0o755); // Ensure execute bit is set.
+}
---
+++ npm/postinstall.js
@@ -0,0 +1,45 @@
+let fs = require('fs');
+let path = require('path');
+
+let parts = [process.platform, process.arch];
+if (process.platform === 'linux') {
+  const {MUSL, family} = require('detect-libc');
+  if (family === MUSL) {
+    parts.push('musl');
+  } else if (process.arch === 'arm') {
+    parts.push('gnueabihf');
+  } else {
+    parts.push('gnu');
+  }
+} else if (process.platform === 'win32') {
+  parts.push('msvc');
+}
+
+let binary = process.platform === 'win32' ? 'sg.exe' : 'sg';
+
+let pkgPath;
+try {
+  pkgPath = path.dirname(require.resolve(`@ast-grep/cli-${parts.join('-')}/package.json`));
+} catch (err) {
+  pkgPath = path.join(__dirname, '..', 'target', 'release');
+  if (!fs.existsSync(path.join(pkgPath, binary))) {
+    pkgPath = path.join(__dirname, '..', 'target', 'debug');
+  }
+}
+
+try {
+  fs.linkSync(path.join(pkgPath, binary), path.join(__dirname, binary));
+} catch (err) {
+  try {
+    fs.copyFileSync(path.join(pkgPath, binary), path.join(__dirname, binary));
+  } catch (err) {
+    console.error('Failed to move @ast-grep/cli binary into place.');
+    process.exit(1);
+  }
+}
+
+if (process.platform === 'win32') {
+  try {
+    fs.unlinkSync(path.join(__dirname, 'sg'));
+  } catch (err) {}
+}
