387bc7ad62a0ccf5e438502621c842d107f720a5
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-09-09 23:22:59 -0400

    [feat] enable change language

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -3,11 +3,11 @@ mod utils;
 use ast_grep_config::{deserialize_rule, SerializableRule};
 use ast_grep_core::language::Language;
 
-use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
 use tree_sitter as ts;
-
 use wasm_bindgen::prelude::*;
+
+use std::sync::Mutex;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -18,7 +18,7 @@ pub struct MatchResult {
   pub end: usize,
 }
 
-static INSTANCE: OnceCell<ts::Language> = OnceCell::new();
+static INSTANCE: Mutex<Option<ts::Language>> = Mutex::new(None);
 
 #[wasm_bindgen]
 pub async fn setup_parser(parser_path: String) -> Result<(), JsError> {
@@ -26,18 +26,21 @@ pub async fn setup_parser(parser_path: String) -> Result<(), JsError> {
   let mut parser = ts::Parser::new()?;
   let lang = get_lang(parser_path).await?;
   parser.set_language(&lang)?;
-  INSTANCE
-    .set(lang)
-    .expect_throw("set current language error");
+  let mut curr_lang = INSTANCE.lock().expect_throw("set language error");
+  *curr_lang = Some(lang);
   Ok(())
 }
 
 #[wasm_bindgen]
 pub async fn find_nodes(src: String, config: JsValue) -> Result<String, JsError> {
   let config: SerializableRule = config.into_serde()?;
-  let lang = INSTANCE.get().expect_throw("current language is not set");
+  let lang = INSTANCE
+    .lock()
+    .expect_throw("get language error")
+    .clone()
+    .expect_throw("current language is not set");
   let root = lang.ast_grep(src);
-  let matcher = deserialize_rule(config, lang.clone())?;
+  let matcher = deserialize_rule(config, lang)?;
   let ret: Vec<_> = root
     .root()
     .find_all(matcher)
