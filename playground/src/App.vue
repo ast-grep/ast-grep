5bb2bb8673291743d940fdf947e639528866b2c2
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-09-11 02:06:05 -0400

    [playground] add loading animation

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -10,7 +10,14 @@ import Intro from './components/Intro.vue'
       <Playground/>
       <template #fallback>
         <div class="loading">
-          Loading Editor and Parser...
+          <svg class="lightning-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="120" viewBox="0 10 100 120">
+            <g transform="matrix(1,0,0,1,0,22)" >
+            <polygon xmlns="http://www.w3.org/2000/svg" points="100,0 25,50 43.75,62.5 0,100 75,62.5 56.25,50"></polygon>
+            </g>
+          </svg>
+          <h2>
+            Loading Editor and Parser...
+          </h2>
         </div>
       </template>
     </Suspense>
@@ -40,7 +47,53 @@ import Intro from './components/Intro.vue'
 .loading {
   display: flex;
   height: 100%;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+
+h2 {
+  font-size: 20px;
+  margin-top: 5vh;
+}
+
+@keyframes lightning-flash {
+  0% {
+    stroke-dashoffset: 1000;
+    fill: #ffe91000;
+  }
+  20% {
+    stroke-dashoffset: 800;
+    fill: #ffe91000;
+  }
+  50% {
+    stroke-dashoffset: 0;
+    fill: #ffe910ff;
+    opacity: 1;
+  }
+  60% {
+    filter: drop-shadow(0 0 0px #fcf1ab00);
+    opacity: 0;
+  }
+  70% {
+    opacity: 1;
+  }
+  80% {
+    opacity: 0;
+  }
+  90% {
+    opacity: 1;
+  }
+  100% {
+    filter: drop-shadow(0 0 20px #fcf1abff);
+  }
+}
+.lightning-icon {
+  fill: #ffe910ff;
+  stroke-dasharray: 1000;
+  stroke-width: 2px;
+  stroke: var(--theme-highlight3);
+  transform: rotate(-35deg);
+  animation: linear 4s lightning-flash forwards infinite;
 }
 </style>
