87c2d47f563c5447263a316b5e1bf3929f3b10bc
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-10-12 12:13:18 -0400

    [feat] add some test command

--- crates/cli/src/test.rs
+++ crates/cli/src/test.rs
@@ -1,6 +1,30 @@
 use clap::Args;
-
+use serde::{Serialize, Deserialize};
 use anyhow::Result;
+
+#[derive(Serialize)]
+struct TestCase {
+  pub id: String,
+  #[serde(default)]
+  pub valid: Vec<String>,
+  #[serde(default)]
+  pub invalid: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Issue {
+  message: String,
+  start: usize,
+  end: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+struct TestSnapshot {
+  pub id: String,
+  pub source: String,
+  pub fixed: Option<String>,
+  pub issues: Vec<Issue>,
+}
 
 /// TODO: add test arguments
 #[derive(Args)]
