5d8e915406cba7c9264a598cb6346ed75ded2ecc
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-11-25 11:28:13 +0800

    test: add test for all/any

--- crates/core/src/ops.rs
+++ crates/core/src/ops.rs
@@ -273,6 +273,14 @@ impl<L: Language, M: Matcher<L>> Op<L, M> {
     }
   }
 
+  pub fn all<MS: IntoIterator<Item = M>>(patterns: MS) -> All<L, M> {
+    All::new(patterns)
+  }
+
+  pub fn any<MS: IntoIterator<Item = M>>(patterns: MS) -> Any<L, M> {
+    Any::new(patterns)
+  }
+
   pub fn new(matcher: M) -> Op<L, M> {
     Self {
       inner: matcher,
@@ -457,6 +465,42 @@ mod test {
     assert_eq!(matcher.potential_kinds().map(|v| v.len()), Some(2));
     // two None kinds
     let matcher = Op::either(Op::not("let a = $_".t())).or(Op::not("let a = 123".t()));
+    assert_eq!(matcher.potential_kinds(), None);
+  }
+
+  #[test]
+  fn test_all_kinds() {
+    // intersect None kinds
+    let matcher = Op::all(["let a = $_".t(), "$A".t()]);
+    assert_eq!(matcher.potential_kinds().map(|v| v.len()), Some(1));
+    let matcher = Op::all(["$A".t(), "let a = $_".t()]);
+    assert_eq!(matcher.potential_kinds().map(|v| v.len()), Some(1));
+    // intersect Same kinds
+    let matcher = Op::all(["let a = $_".t(), "let b = 123".t()]);
+    assert_eq!(matcher.potential_kinds().map(|v| v.len()), Some(1));
+    // intersect different kinds
+    let matcher = Op::all(["let a = 1".t(), "console.log(1)".t()]);
+    assert_eq!(matcher.potential_kinds().map(|v| v.len()), Some(0));
+    // two None kinds
+    let matcher = Op::all(["$A".t(), "$B".t()]);
+    assert_eq!(matcher.potential_kinds(), None);
+  }
+
+  #[test]
+  fn test_any_kinds() {
+    // union None kinds
+    let matcher = Op::any(["let a = $_".t(), "$A".t()]);
+    assert_eq!(matcher.potential_kinds(), None);
+    let matcher = Op::any(["$A".t(), "let a = $_".t()]);
+    assert_eq!(matcher.potential_kinds(), None);
+    // union Same kinds
+    let matcher = Op::any(["let a = $_".t(), "let b = 123".t()]);
+    assert_eq!(matcher.potential_kinds().map(|v| v.len()), Some(1));
+    // union different kinds
+    let matcher = Op::any(["let a = 1".t(), "console.log(1)".t()]);
+    assert_eq!(matcher.potential_kinds().map(|v| v.len()), Some(2));
+    // two None kinds
+    let matcher = Op::any(["$A".t(), "$B".t()]);
     assert_eq!(matcher.potential_kinds(), None);
   }
 }
