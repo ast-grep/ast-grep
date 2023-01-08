0323dcabd9f65148bb10b9f999cbf20d8eeab837
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-01-08 13:45:52 +0800

    feat: add git check for release automation

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-  println!("hello")
+use anyhow::{bail, Result};
+use std::process::{Command, Stdio};
+
+fn main() -> Result<()> {
+  check_git_status()?;
+  update_npm()?;
+  update_napi()?;
+  update_crates()?;
+  Ok(())
+}
+
+fn check_git_status() -> Result<()> {
+  let git = Command::new("git")
+    .arg("status")
+    .arg("--porcelain")
+    .stdout(Stdio::piped())
+    .spawn()?
+    .wait_with_output()?;
+  if git.stdout.len() > 0 {
+    bail!("The git working directory has uncommitted changes. Please commit or abandon them before release!")
+  } else {
+    Ok(())
+  }
+}
+
+fn update_npm() -> Result<()> {
+  Ok(())
+}
+
+fn update_napi() -> Result<()> {
+  Ok(())
+}
+
+fn update_crates() -> Result<()> {
+  Ok(())
 }
