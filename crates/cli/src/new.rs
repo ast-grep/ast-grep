c54b0a5934dc95b45ddb43c66835d3f6b270ad02
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-03-08 20:58:23 +0800

    feat: add some skeleton code for new

--- crates/cli/src/new.rs
+++ crates/cli/src/new.rs
@@ -7,7 +7,7 @@ pub struct NewArg {
   entity: Option<Entity>,
 }
 
-#[derive(Subcommand, Debug)]
+#[derive(Subcommand, Debug, PartialEq, Eq)]
 enum Entity {
   Project,
   Rule,
@@ -24,10 +24,39 @@ pub fn run_create_new(arg: NewArg) -> Result<()> {
   Ok(())
 }
 
-fn run_create_entity(entity: Entity) {
-  println!("{entity:?}")
+fn is_cwd_under_a_project() -> bool {
+  todo!()
 }
 
+fn run_create_entity(entity: Entity) {
+  // check if we creating a project
+  if entity == Entity::Project {
+    // create the project if user choose to create
+    println!("creating project!");
+    todo!()
+  }
+  // check if we are under a project dir
+  if !is_cwd_under_a_project() {
+    // if not, return error
+    return;
+  }
+  // ask user what destination to create if multiple dirs exist
+  println!("{entity:?}");
+  // ask if a test is needed if user is creating a rule
+  if entity == Entity::Rule {
+    println!("Do you also need to create a rule?");
+    todo!()
+  }
+}
+
+// TODO:
+// 1. check if we are under a sgconfig.yml
+// 2. ask users what to create if yes
+// 3. ask users to provide project info if no sgconfig found
 fn ask_entity_type() {
-  println!("What do you want to create?")
+  if is_cwd_under_a_project() {
+    println!("What do you want to create?");
+  } else {
+    println!("creating project!");
+  }
 }
