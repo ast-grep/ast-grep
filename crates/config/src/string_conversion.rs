dac3f6ec0391f8c1c2f97237e9491454d6b30487
Aarni Koskela <akx@iki.fi>
2023-09-12 15:56:11 +0300

    Implement string conversions described in #436 as a stand-alone chainable `convert` transform

---
+++ crates/config/src/string_conversion.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Copy)]
+#[serde(rename_all = "lowercase")]
+pub enum StringConversion {
+  LowerCase,
+  UpperCase,
+  Capitalize,
+}
+
+pub fn apply_string_conversion(string: String, transform: &StringConversion) -> String {
+  match transform {
+    StringConversion::LowerCase => string.to_lowercase(),
+    StringConversion::UpperCase => string.to_uppercase(),
+    StringConversion::Capitalize => {
+      let mut chars = string.chars();
+      if let Some(c) = chars.next() {
+        c.to_uppercase().chain(chars).collect()
+      } else {
+        string
+      }
+    },
+  }
+}
