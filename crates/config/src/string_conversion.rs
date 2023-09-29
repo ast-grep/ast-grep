710f76ea42106bda4003675a3b31b5e3b1bc0d9c
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-09-29 09:00:20 -0700

    fix: remove unused func temporarily

--- crates/config/src/string_conversion.rs
+++ crates/config/src/string_conversion.rs
@@ -30,69 +30,69 @@ impl StringFormat {
       _ => todo!(),
     }
   }
-  pub fn split(&self, string: &str) -> Vec<String> {
-    match &self {
-      StringFormat::CamelCase => split_by_capital_letters(string),
-      StringFormat::SnakeCase => split_snake_case(string),
-      StringFormat::KebabCase => split_kebab_case(string),
-      StringFormat::PascalCase => split_by_capital_letters(string),
-      _ => todo!(),
-    }
-  }
-  pub fn join(&self, words: &[String]) -> String {
-    match &self {
-      StringFormat::CamelCase => join_camel_case(words),
-      StringFormat::SnakeCase => words.join("_"),
-      StringFormat::KebabCase => words.join("-"),
-      StringFormat::PascalCase => words
-        .iter()
-        .map(|s| capitalize(s))
-        .collect::<Vec<_>>()
-        .join(""),
-      _ => todo!(),
-    }
-  }
+  // pub fn split(&self, string: &str) -> Vec<String> {
+  //   match &self {
+  //     StringFormat::CamelCase => split_by_capital_letters(string),
+  //     StringFormat::SnakeCase => split_snake_case(string),
+  //     StringFormat::KebabCase => split_kebab_case(string),
+  //     StringFormat::PascalCase => split_by_capital_letters(string),
+  //     _ => todo!(),
+  //   }
+  // }
+  // pub fn join(&self, words: &[String]) -> String {
+  //   match &self {
+  //     StringFormat::CamelCase => join_camel_case(words),
+  //     StringFormat::SnakeCase => words.join("_"),
+  //     StringFormat::KebabCase => words.join("-"),
+  //     StringFormat::PascalCase => words
+  //       .iter()
+  //       .map(|s| capitalize(s))
+  //       .collect::<Vec<_>>()
+  //       .join(""),
+  //     _ => todo!(),
+  //   }
+  // }
 }
 
-fn split_words(s: &str) -> Vec<String> {
-  s.split_whitespace().map(|s| s.to_string()).collect()
-}
+// fn split_words(s: &str) -> Vec<String> {
+//   s.split_whitespace().map(|s| s.to_string()).collect()
+// }
 
-fn join_camel_case(words: &[String]) -> String {
-  let mut result = String::new();
-  for (i, word) in words.iter().enumerate() {
-    if i == 0 {
-      result.push_str(&word.to_lowercase());
-    } else {
-      result.push_str(&capitalize(word));
-    }
-  }
-  result
-}
+// fn join_camel_case(words: &[String]) -> String {
+//   let mut result = String::new();
+//   for (i, word) in words.iter().enumerate() {
+//     if i == 0 {
+//       result.push_str(&word.to_lowercase());
+//     } else {
+//       result.push_str(&capitalize(word));
+//     }
+//   }
+//   result
+// }
 
-fn split_by_capital_letters(camel_string: &str) -> Vec<String> {
-  let mut words = vec![];
-  let mut word = String::new();
-  for c in camel_string.chars() {
-    if c.is_uppercase() {
-      if !word.is_empty() {
-        words.push(word);
-      }
-      word = String::new();
-    }
-    word.push(c.to_ascii_lowercase());
-  }
-  words.push(word);
-  words
-}
+// fn split_by_capital_letters(camel_string: &str) -> Vec<String> {
+//   let mut words = vec![];
+//   let mut word = String::new();
+//   for c in camel_string.chars() {
+//     if c.is_uppercase() {
+//       if !word.is_empty() {
+//         words.push(word);
+//       }
+//       word = String::new();
+//     }
+//     word.push(c.to_ascii_lowercase());
+//   }
+//   words.push(word);
+//   words
+// }
 
-fn split_snake_case(snake_string: &str) -> Vec<String> {
-  snake_string.split('_').map(|s| s.to_string()).collect()
-}
+// fn split_snake_case(snake_string: &str) -> Vec<String> {
+//   snake_string.split('_').map(|s| s.to_string()).collect()
+// }
 
-fn split_kebab_case(kebab_string: &str) -> Vec<String> {
-  kebab_string.split('-').map(|s| s.to_string()).collect()
-}
+// fn split_kebab_case(kebab_string: &str) -> Vec<String> {
+//   kebab_string.split('-').map(|s| s.to_string()).collect()
+// }
 
 #[cfg(test)]
 mod test {
