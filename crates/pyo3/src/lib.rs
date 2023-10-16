1a6b22e7acb65789e6379f7c14a0bea0b549ece0
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-10-15 20:45:45 -0700

    feat: initial support for SgRoot in Python!
    part of #389

--- crates/pyo3/src/lib.rs
+++ crates/pyo3/src/lib.rs
@@ -1,16 +1,43 @@
 #![cfg(not(test))]
 #![cfg(feature = "pyo3")]
+use ast_grep_core::{AstGrep, Language, StrDoc};
+use ast_grep_language::SupportLang;
 use pyo3::prelude::*;
-
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-  Ok((a + b).to_string())
-}
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn ast_grep_pyo3(_py: Python, m: &PyModule) -> PyResult<()> {
-  m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+  m.add_class::<SgRoot>()?;
+  m.add_class::<SgNode>()?;
   Ok(())
 }
+
+#[pyclass]
+struct SgRoot {
+  inner: AstGrep<StrDoc<SupportLang>>,
+  filename: String,
+}
+
+#[pymethods]
+impl SgRoot {
+  #[new]
+  fn new(src: &str, lang: &str) -> Self {
+    let lang: SupportLang = lang.parse().unwrap();
+    let inner = lang.ast_grep(src);
+    Self {
+      inner,
+      filename: "anonymous".into(),
+    }
+  }
+
+  fn root(&self) -> SgNode {
+    SgNode {}
+  }
+
+  fn filename(&self) -> &str {
+    &self.filename
+  }
+}
+
+#[pyclass]
+struct SgNode {}
