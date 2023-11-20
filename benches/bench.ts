775cee48dd57b129e7e19e78665752a2fcdf2452
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-11-19 17:41:06 -0800

    refactor: better reusability code for benchmark

--- benches/bench.ts
+++ benches/bench.ts
@@ -10,47 +10,61 @@ import Parser from 'tree-sitter'
 // because tree-sitter-typescript does not have d.ts
 const tresSitterTS = require('tree-sitter-typescript').typescript
 
-async function run() {
-  const treeSitter = new Parser()
-  treeSitter.setLanguage(tresSitterTS)
+const treeSitter = new Parser()
+treeSitter.setLanguage(tresSitterTS)
+
+function prepareCases() {
   const tsEntry = fs.readFileSync('./fixtures/tsc.ts.fixture', 'utf8')
   const vueRef = fs.readFileSync('./fixtures/ref.ts.fixture', 'utf8')
   const tsChecker = fs.readFileSync('./fixtures/checker.ts.fixture', 'utf8')
-  const cases = [
+  return [
     ['Parse One Line', 'let a = 123'],
     ['Parse Small File', tsEntry],
     ['Parse Medium File', vueRef],
     ['Parse Huge File', tsChecker],
   ]
+}
+
+function parseSyncBench(source: string) {
+  return {
+    'ast-grep parse': () => {
+      sg.parse(source)
+    },
+    'tree-sitter parse': () => {
+      treeSitter.parse(source)
+    },
+    'babel parse': () => {
+      babel.parse(source, {
+        plugins: ['typescript'],
+        sourceType: 'module',
+      })
+    },
+    'oxc parse': () => {
+      oxc.parseSync(source, {
+        sourceType: 'module',
+        sourceFilename: 'test.ts',
+      })
+    },
+    'swc parse': () => {
+      swc.parseSync(source, {
+        syntax: 'typescript',
+      })
+    },
+    'TypeScript parse': () => {
+      ts.createSourceFile('benchmark.ts', source, ts.ScriptTarget.Latest)
+    },
+  }
+}
+
+async function run() {
+  const cases = prepareCases()
   for (const [title, source] of cases) {
+    const benches = parseSyncBench(source)
     await b.suite(
       title,
-      b.add('ast-grep parse', () => {
-        sg.parse(source)
-      }),
-      b.add('tree-sitter parse', () => {
-        treeSitter.parse(source)
-      }),
-      b.add('babel parse', () => {
-        babel.parse(source, {
-          plugins: ['typescript'],
-          sourceType: 'module',
-        })
-      }),
-      b.add('oxc parse', () => {
-        oxc.parseSync(source, {
-          sourceType: 'module',
-          sourceFilename: 'test.ts',
-        })
-      }),
-      b.add('swc parse', () => {
-        swc.parseSync(source, {
-          syntax: 'typescript',
-        })
-      }),
-      b.add('TypeScript parse', () => {
-        ts.createSourceFile('benchmark.ts', source, ts.ScriptTarget.Latest)
-      }),
+      ...Object.entries(benches).map(([runnerName, runner]) =>
+        b.add(runnerName, runner),
+      ),
       b.cycle(),
       b.complete(),
     )
