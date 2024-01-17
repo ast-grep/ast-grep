43d5d4bf6866a89775642cfae83a23e5d9e331ec
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-01-17 01:13:49 -0800

    feat: add applyRewriters transformation

--- crates/config/src/transform/rewriters.rs
+++ crates/config/src/transform/rewriters.rs
@@ -1,19 +1,47 @@
 use super::Ctx;
 
-use ast_grep_core::Doc;
+use ast_grep_core::meta_var::MetaVariable;
+use ast_grep_core::source::Content;
+use ast_grep_core::{Doc, Language, Node};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct Rewriters {
+  source: String,
   rewrites: Vec<String>,
+  // do we need this?
+  // sort_by: Option<String>,
   join_by: Option<String>,
 }
 
-impl Rewriters {
-  pub fn compute<D: Doc>(&self, ctx: &mut Ctx<D>) -> Option<String> {
-    // TODO
-    None
+fn get_nodes_from_env<'b, D: Doc>(var: &MetaVariable, ctx: &Ctx<'_, 'b, D>) -> Vec<Node<'b, D>> {
+  match var {
+    MetaVariable::MultiCapture(n) => ctx.env.get_multiple_matches(n),
+    MetaVariable::Capture(m, _) => {
+      if let Some(n) = ctx.env.get_match(m) {
+        vec![n.clone()]
+      } else {
+        vec![]
+      }
+    }
+    _ => vec![],
   }
+}
+
+impl Rewriters {
+  pub(super) fn compute<D: Doc>(&self, ctx: &mut Ctx<D>) -> Option<String> {
+    let source = ctx.lang.pre_process_pattern(&self.source);
+    let var = ctx.lang.extract_meta_var(&source)?;
+    let nodes = get_nodes_from_env(&var, ctx);
+    let bytes = ctx.env.get_var_bytes(&var)?;
+    let _ = find_and_make_edits(bytes, nodes);
+    todo!()
+  }
+}
+
+type Bytes<D> = [<<D as Doc>::Source as Content>::Underlying];
+fn find_and_make_edits<D: Doc>(_bytes: &Bytes<D>, _nodes: Vec<Node<D>>) -> Option<()> {
+  todo!()
 }
