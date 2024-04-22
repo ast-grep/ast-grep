eec9eb35ba7af4be69255b1a9c9f1a129786ad7d
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-04-22 01:21:59 -0700

    feat: export used meta-var in transform
    fix #1069

--- crates/config/src/transform/rewrite.rs
+++ crates/config/src/transform/rewrite.rs
@@ -10,7 +10,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct Rewrite {
-  source: String,
+  pub(super) source: String,
   rewriters: Vec<String>,
   // do we need this?
   // sort_by: Option<String>,
