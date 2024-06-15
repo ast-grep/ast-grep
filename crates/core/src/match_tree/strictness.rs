2d2c4c1e0384636d3c13b30c4227d37c7bea7e48
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-06-15 13:32:29 -0700

    fix: add strictness in match_node

--- crates/core/src/match_tree/strictness.rs
+++ crates/core/src/match_tree/strictness.rs
@@ -1,7 +1,32 @@
-enum MatchStrictness {
+use crate::{Doc, Node};
+
+pub enum MatchStrictness {
   Cst,         // all nodes are matched
   Smart,       // all nodes except source trivial nodes are matched.
   Significant, // only significant nodes are matched
   Ast,         // only ast nodes are matched
   Lenient,     // ast-nodes excluding comments are matched
+}
+
+impl MatchStrictness {
+  pub fn should_skip_matching_node<D: Doc>(&self, node: &Node<D>) -> bool {
+    use MatchStrictness::*;
+    match self {
+      Cst => todo!(),
+      Smart => !node.is_named(),
+      Significant => todo!(),
+      Ast => todo!(),
+      Lenient => todo!(),
+    }
+  }
+  pub fn should_keep_in_pattern<D: Doc>(&self, node: &Node<D>) -> bool {
+    use MatchStrictness::*;
+    match self {
+      Cst => true,
+      Smart => true,
+      Significant => todo!("named + has field"),
+      Ast => node.is_named(),
+      Lenient => todo!("skip comment like"),
+    }
+  }
 }
