dce455d40c53cb3f8a2a0c80bb57c99df46ad035
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-06-15 10:34:39 -0700

    feat: add strctness enum

---
+++ crates/core/src/match_tree/strictness.rs
@@ -0,0 +1,7 @@
+enum MatchAlgorithm {
+  Cst,         // all nodes are matched
+  Smart,       // all nodes except source trivial nodes are matched.
+  Significant, // only significant nodes are matched
+  Ast,         // only ast nodes are matched
+  Lenient,     // ast-nodes excluding comments are matched
+}
