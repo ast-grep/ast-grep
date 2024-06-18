9ac1f76989d4d7b1fdde8df3e72b26dd260c1cc3
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-06-17 23:19:54 -0400

    feat: add more strictness

--- crates/core/src/match_tree/strictness.rs
+++ crates/core/src/match_tree/strictness.rs
@@ -1,22 +1,22 @@
 use crate::{Doc, Node};
 
 pub enum MatchStrictness {
-  Cst,         // all nodes are matched
-  Smart,       // all nodes except source trivial nodes are matched.
-  Significant, // only significant nodes are matched
-  Ast,         // only ast nodes are matched
-  Lenient,     // ast-nodes excluding comments are matched
+  Cst,       // all nodes are matched
+  Smart,     // all nodes except source trivial nodes are matched.
+  Ast,       // only ast nodes are matched
+  Lenient,   // ast-nodes excluding comments are matched
+  Signature, // ast-nodes excluding comments, without text
 }
 
 impl MatchStrictness {
   pub fn should_skip_matching_node<D: Doc>(&self, node: &Node<D>) -> bool {
     use MatchStrictness::*;
     match self {
-      Cst => todo!(),
+      Cst => false,
       Smart => !node.is_named(),
-      Significant => todo!(),
-      Ast => todo!(),
-      Lenient => todo!(),
+      Ast => !node.is_named(),
+      Lenient => !node.is_named() || node.is_comment_like(),
+      Signature => !node.is_named() || node.is_comment_like(),
     }
   }
   pub fn should_keep_in_pattern<D: Doc>(&self, node: &Node<D>) -> bool {
@@ -24,9 +24,9 @@ impl MatchStrictness {
     match self {
       Cst => true,
       Smart => true,
-      Significant => todo!("named + has field"),
       Ast => node.is_named(),
-      Lenient => todo!("skip comment like"),
+      Lenient => node.is_named() && !node.is_comment_like(),
+      Signature => node.is_named() && !node.is_comment_like(),
     }
   }
 }
