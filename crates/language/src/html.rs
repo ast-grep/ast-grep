010a63e7837d54aece8cd42f61b39cf8b4d32394
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-07-08 00:29:17 -0700

    feat: implement rough html language

--- crates/language/src/html.rs
+++ crates/language/src/html.rs
@@ -1,6 +1,6 @@
 use super::pre_process_pattern;
-use ast_grep_core::language::TSRange;
-use ast_grep_core::{Doc, Node};
+use ast_grep_core::language::{TSPoint, TSRange};
+use ast_grep_core::{matcher::KindMatcher, Doc, Node};
 
 // tree-sitter-html uses locale dependent iswalnum for tagName
 // https://github.com/tree-sitter/tree-sitter-html/blob/b5d9758e22b4d3d25704b72526670759a9e4d195/src/scanner.c#L194
@@ -19,9 +19,36 @@ impl ast_grep_core::language::Language for Html {
   fn injectable_languages(&self) -> Option<&'static [&str]> {
     Some(&["css", "javascript"])
   }
-  fn extract_injections<D: Doc>(&self, root: Node<D>) -> Vec<(String, Vec<TSRange>)> {
-    vec![]
+  fn extract_injections<D: Doc<Lang = Self>>(&self, root: Node<D>) -> Vec<(String, Vec<TSRange>)> {
+    let mut scripts = vec![];
+    let matcher = KindMatcher::new("script_element", *self);
+    for script in root.find_all(matcher) {
+      for child in script.children() {
+        if child.kind() == "raw_text" {
+          scripts.push(node_to_range(&child));
+        }
+      }
+    }
+    let mut styles = vec![];
+    let matcher = KindMatcher::new("style_element", *self);
+    for style in root.find_all(matcher) {
+      for child in style.children() {
+        if child.kind() == "raw_text" {
+          styles.push(node_to_range(&child));
+        }
+      }
+    }
+    vec![("css".into(), styles), ("javascript".into(), scripts)]
   }
+}
+
+fn node_to_range<D: Doc>(node: &Node<D>) -> TSRange {
+  let r = node.range();
+  let start = node.start_pos();
+  let sp = TSPoint::new(start.0 as u32, start.1 as u32);
+  let end = node.end_pos();
+  let ep = TSPoint::new(end.0 as u32, end.1 as u32);
+  TSRange::new(r.start as u32, r.end as u32, &sp, &ep)
 }
 
 #[cfg(test)]
