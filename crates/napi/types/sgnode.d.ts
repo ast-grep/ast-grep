627950413fafc4cf155fbf4361350a58a509ee0b
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-12-16 23:47:46 -0500

    feat: add kindToRefine field and RefineNode interface

--- crates/napi/types/sgnode.d.ts
+++ crates/napi/types/sgnode.d.ts
@@ -34,7 +34,7 @@ export interface Range {
 
 export declare class SgNode<
   M extends NodeTypesMap = NodeTypesMap,
-  T extends NodeKinds<M> = NodeKinds<M>,
+  out T extends NodeKinds<M> = NodeKinds<M>,
 > {
   range(): Range
   isLeaf(): boolean
@@ -42,15 +42,16 @@ export declare class SgNode<
   isNamedLeaf(): boolean
   /** Returns the string name of the node kind */
   kind(): T
+  readonly kindToRefine: T
   /** Check if the node is the same kind as the given `kind` string */
-  is<K extends T>(kind: K): this is SgNode<M, K>
+  is<K extends NodeKinds<M>>(kind: K): this is SgNode<M, K>
   text(): string
   matches(m: string): boolean
   inside(m: string): boolean
   has(m: string): boolean
   precedes(m: string): boolean
   follows(m: string): boolean
-  getMatch<K extends NodeKinds<M>>(m: string): SgNode<M, K> | null
+  getMatch<K extends NodeKinds<M>>(m: string): RefineNode<M, K> | null
   getMultipleMatches(m: string): Array<SgNode<M>>
   getTransformed(m: string): string | null
   /** Returns the node's SgRoot */
@@ -60,22 +61,22 @@ export declare class SgNode<
   id(): number
   find<K extends NodeKinds<M>>(
     matcher: string | number | NapiConfig<M>,
-  ): SgNode<M, K> | null
+  ): RefineNode<M, K> | null
   findAll<K extends NodeKinds<M>>(
     matcher: string | number | NapiConfig<M>,
-  ): Array<SgNode<M, K>>
+  ): Array<RefineNode<M, K>>
   /** Finds the first child node in the `field` */
-  field<F extends FieldNames<M[T]>>(name: F): FieldSgNode<M, T, F>
+  field<F extends FieldNames<M[T]>>(name: F): FieldNode<M, T, F>
   /** Finds all the children nodes in the `field` */
   fieldChildren<F extends FieldNames<M[T]>>(
     name: F,
-  ): NonNullable<FieldSgNode<M, T, F>>[]
-  parent<K extends NodeKinds<M>>(): SgNode<M, K> | null
-  child<K extends NodeKinds<M>>(nth: number): SgNode<M, K> | null
+  ): Exclude<FieldNode<M, T, F>, null>[]
+  parent<K extends NodeKinds<M>>(): RefineNode<M, K> | null
+  child<K extends NodeKinds<M>>(nth: number): RefineNode<M, K> | null
   ancestors(): Array<SgNode<M>>
-  next<K extends NodeKinds<M>>(): SgNode<M, K> | null
+  next<K extends NodeKinds<M>>(): RefineNode<M, K> | null
   nextAll(): Array<SgNode<M>>
-  prev<K extends NodeKinds<M>>(): SgNode<M, K> | null
+  prev<K extends NodeKinds<M>>(): RefineNode<M, K> | null
   prevAll(): Array<SgNode<M>>
   replace(text: string): Edit
   commitEdits(edits: Array<Edit>): string
@@ -83,7 +84,7 @@ export declare class SgNode<
 /** Represents the parsed tree of code. */
 export declare class SgRoot<M extends NodeTypesMap = NodeTypesMap> {
   /** Returns the root SgNode of the ast-grep instance. */
-  root<K extends NodeKinds<M> = RootKind<M>>(): SgNode<M, K>
+  root(): SgNode<M, RootKind<M>>
   /**
    * Returns the path of the file if it is discovered by ast-grep's `findInFiles`.
    * Returns `"anonymous"` if the instance is created by `lang.parse(source)`.
@@ -91,11 +92,19 @@ export declare class SgRoot<M extends NodeTypesMap = NodeTypesMap> {
   filename(): string
 }
 
-type FieldSgNode<
+/**
+ * if K contains string, return general SgNode. Otherwise,
+ * if K is a literal union, return a union of SgNode of each kind.
+ */
+type RefineNode<M extends NodeTypesMap, K> =
+  string extends K ? SgNode<M> :
+    K extends NodeKinds<M> ? SgNode<M, K> : never
+
+type FieldNode<
   M extends NodeTypesMap,
   K extends NodeKinds<M>,
   F extends FieldNames<M[K]>,
   I extends NodeFieldInfo = ExtractField<M[K], F>,
 > = I extends { required: true }
-  ? SgNode<M, TypesInField<M, I>>
-  : SgNode<M, TypesInField<M, I>> | null
+  ? RefineNode<M, TypesInField<M, I>>
+  : RefineNode<M, TypesInField<M, I>> | null
