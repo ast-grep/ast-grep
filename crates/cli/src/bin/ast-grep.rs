eb4a7c5a403dffdb56bffa0e33efab7148caa941
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2025-01-22 09:51:39 -0500

    feat: compile sg as an alias of ast-grep
    this change direct sg command arg/stdio/exit status code to ast-grep
    
    fix #1757

--- crates/cli/src/bin/ast-grep.rs
+++ crates/cli/src/bin/ast-grep.rs
@@ -1,9 +1,16 @@
-// This file is exactly the same as main.rs
+// This command `sg` redirects everything to ast-grep
 // we need this to avoid "multiple build target" warning
 // See https://github.com/rust-lang/cargo/issues/5930
-use anyhow::Result;
-use ast_grep::execute_main;
-
-fn main() -> Result<()> {
-  execute_main()
+fn main() -> std::io::Result<()> {
+  // redirect to ast-grep
+  use std::env::args;
+  use std::process::{Command, Stdio};
+  let mut child = Command::new("ast-grep")
+    .args(args().skip(1))
+    .stdin(Stdio::inherit())
+    .stdout(Stdio::inherit())
+    .stderr(Stdio::inherit())
+    .spawn()?;
+  let status = child.wait()?;
+  std::process::exit(status.code().unwrap_or(1))
 }
