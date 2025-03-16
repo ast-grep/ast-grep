fb48a1f10ef6180b2ebe2e027e21364262f78281
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2025-03-16 14:48:58 -0400

    fix: simplify build script

--- crates/wasm/build.sh
+++ crates/wasm/build.sh
@@ -3,20 +3,9 @@
 export CFLAGS_wasm32_unknown_unknown="-I$(pwd)/wasm-sysroot -Wbad-function-cast -Wcast-function-type -fno-builtin" RUSTFLAGS="-Zwasm-c-abi=spec"
 export AST_GREP_RULE='{"id":"fix-wasm-js-node","language":"javascript","rule":{"pattern":"module_or_path = fetch(module_or_path);","inside":{"pattern":"function __wbg_init($$$) {$$$}","stopBy":"end"}},"fix":"if (!!process.versions.node) {\n  const fs = await import(\"fs/promises\");\n  module_or_path = fs.readFile(module_or_path);\n} else {\n  module_or_path = fetch(module_or_path);\n}\n"}'
 
-# Build with exhaustive-lang feature
-export OUT_DIR=pkg-exhaustive
-wasm-pack build -s ast-grep --release --target web --out-dir $OUT_DIR --features "exhaustive-lang" \
-	-Z build-std=panic_abort,std -Z build-std-features=panic_immediate_abort
-
-# Make the WASM module work with Node.js as Node does not support fetch for file:// URLs
-cargo run --manifest-path ../cli/Cargo.toml -- scan --inline-rules "$AST_GREP_RULE" -U $OUT_DIR/ast_grep_wasm.js
-
-sed -i ".bak" -e 's/"name": "@ast-grep\/ast-grep-wasm"/"name": "@ast-grep\/wasm"/g' $OUT_DIR/package.json
-rm $OUT_DIR/package.json.bak
-
 # Build with small-lang feature
-export OUT_DIR=pkg-small
-wasm-pack build -s ast-grep --release --target web --out-dir $OUT_DIR --features "small-lang" \
+export OUT_DIR=pkg
+wasm-pack build --scope ast-grep --release --target web --out-dir $OUT_DIR \
 	-Z build-std=panic_abort,std -Z build-std-features=panic_immediate_abort
 
 cargo run --manifest-path ../cli/Cargo.toml -- scan --inline-rules "$AST_GREP_RULE" -U $OUT_DIR/ast_grep_wasm.js
