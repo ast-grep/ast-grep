679564a8c3e34abe3dea3c5628e9776be7793828
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2025-03-16 19:47:14 -0400

    fix: remove c_variadic use

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(c_variadic)]
 #![cfg(target_arch = "wasm32")]
 pub mod wasm_libc;
 
--- crates/wasm/wasm-sysroot/stdio.h
+++ crates/wasm/wasm-sysroot/stdio.h
@@ -7,13 +7,16 @@
 #define stdout NULL
 #define stderr NULL
 
-int fprintf(FILE *__restrict__, const char *__restrict__, ...);
+/* int fprintf(FILE *__restrict__, const char *__restrict__, ...); */
 int fputs(const char *__restrict, FILE *__restrict);
 int fputc(int, FILE *);
 FILE *fdopen(int, const char *);
 int fclose(FILE *);
 
-int vsnprintf(char *s, unsigned long n, const char *format, ...);
+/* int vsnprintf(char *s, unsigned long n, const char *format, ...); */
 
+// these are just placeholders and won't be called in wasm
+#define vsnprintf(s, n, format, ...)
+#define fprintf(f, fmt, ...) 0
 #define sprintf(str, ...) 0
 #define snprintf(str, len, ...) 0
