174707ed777843e1dc67f98cda25d0c67a7e3389
Dsaquel <291874700n@gmail.com>
2025-12-12 00:57:55 +0100

    fix: sg completions redirect to ast-grep
    chore: fmt

--- crates/cli/src/bin/alias.rs
+++ crates/cli/src/bin/alias.rs
@@ -1,16 +1,5 @@
-// The alias command `sg` redirects everything to ast-grep
-// we need this to avoid "multiple build target" warning
-// See https://github.com/rust-lang/cargo/issues/5930
-fn main() -> std::io::Result<()> {
-  // redirect to ast-grep
-  use std::env::args;
-  use std::process::{Command, Stdio};
-  let mut child = Command::new("ast-grep")
-    .args(args().skip(1))
-    .stdin(Stdio::inherit())
-    .stdout(Stdio::inherit())
-    .stderr(Stdio::inherit())
-    .spawn()?;
-  let status = child.wait()?;
-  std::process::exit(status.code().unwrap_or(1))
+use ast_grep::execute_main;
+
+fn main() -> anyhow::Result<()> {
+  execute_main()
 }
